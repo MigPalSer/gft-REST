71366d197964dcd880f3ffc70fe7c82da139b839
Miguel Palomino <mlpv@gft.com>
2022-02-22 11:33:04 +0100

    buscar user por id

--- src/main/java/migpalser/cursoRESTGFT/UserResource.java
+++ src/main/java/migpalser/cursoRESTGFT/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,4 +29,8 @@ public class UserResource {
 		udao.save(user);
 	}
 	
+	@GetMapping("/users/{id}")
+	public User userById(@PathVariable int id){
+		return udao.findOne(id);
+	}
 }
